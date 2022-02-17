d98f5b793c848294e564eb9de302aa9e30b7faac
Zaliya Validova <z.validova@yandex.ru>
2022-02-17 18:40:13 +0500

    Добавление 16 дз

--- main.js
+++ main.js
@@ -1,28 +1,21 @@
-const body = document.querySelector('body');
-const DomElement = function(selector, height, width, bg, fontSize) {
-    this.selector = selector;
-    this.height = height;
-    this.width = width;
-    this.bg = bg;
-    this.fontSize = fontSize;
-    this.createElem = function() {
-        let domElem;
-        if (this.selector.includes('.')) {
-            domElem = document.createElement('div');
-            domElem.classList.add(this.selector.replace('.', ''));
-        } else if (this.selector.includes('#')) {
-            domElem = document.createElement('div');
-            domElem.id = this.selector.replace('#', '');
-        } else {
-            domElem = document.createElement(this.selector);
-        }
-        domElem.style.height = this.height;
-        domElem.style.width = this.width;
-        domElem.style.background = this.bg;
-        domElem.style.fontSize = this.fontSize;
-        domElem.textContent = "Как дела?";
-        body.append(domElem);
-        };
-};
-const elem = new DomElement('#div', '30px', '100px', 'green', '20px');
-elem.createElem();
+class First {
+    constructor(name){
+        this.name = name;
+        console.log('Меня зовут ' + name);
+    }
+    hello() {
+        console.log("Привет я метод родителя!");
+    }
+}
+
+class Second extends First {
+    constructor(name){
+        super(name);
+    }
+    myHello() {
+        super.hello();
+        console.log("А я наследуемый метод!");
+    }
+}
+const say = new Second('Залия');
+console.log(say.myHello());
